63f377fe89f71142e69170ac49f3853414073384
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2025-07-14 19:20:18 -0300

    Add files via upload

--- codigo_fuente/Reservas.js
+++ codigo_fuente/Reservas.js
@@ -1,1 +1,12 @@
+// Simulación de módulo de reservas
 
+function crearReserva(usuario, cancha, fecha) {
+  console.log(`Reserva creada para ${usuario} en ${cancha} el ${fecha}`);
+}
+
+function cancelarReserva(idReserva) {
+  console.log(`Reserva ${idReserva} cancelada.`);
+}
+
+// Exportar funciones
+export { crearReserva, cancelarReserva };
